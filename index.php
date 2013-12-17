be5752579d08d585cf7567bb9ca2a82955d130f3
Mazen <mazen@Maz3n.com>
2013-12-17 13:09:39 -0800

    Signed-off-by: Mazen <mazen@Maz3n.com>

--- index.php
+++ index.php
@@ -1,16 +1,40 @@
 <?php
 /**
-* @package   yoo_capture
-* @author    YOOtheme http://www.yootheme.com
-* @copyright Copyright (C) YOOtheme GmbH
-* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
-*/
+ * @package    Joomla.Site
+ *
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-// no direct access
-defined('_JEXEC') or die('Restricted access');
+if (version_compare(PHP_VERSION, '5.3.1', '<'))
+{
+	die('Your host needs to use PHP 5.3.1 or higher to run this version of Joomla!');
+}
 
-// get warp
-$warp = require(__DIR__.'/warp.php');
+/**
+ * Constant that is checked in included files to prevent direct access.
+ * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
+ */
+define('_JEXEC', 1);
 
-// load main theme file, located in /layouts/theme.php
-echo $warp['template']->render('theme');
+if (file_exists(__DIR__ . '/defines.php'))
+{
+	include_once __DIR__ . '/defines.php';
+}
+
+if (!defined('_JDEFINES'))
+{
+	define('JPATH_BASE', __DIR__);
+	require_once JPATH_BASE . '/includes/defines.php';
+}
+
+require_once JPATH_BASE . '/includes/framework.php';
+
+// Mark afterLoad in the profiler.
+JDEBUG ? $_PROFILER->mark('afterLoad') : null;
+
+// Instantiate the application.
+$app = JFactory::getApplication('site');
+
+// Execute the application.
+$app->execute();
